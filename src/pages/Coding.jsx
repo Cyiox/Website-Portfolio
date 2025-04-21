8e56744f3423e34b21102079709ce731e96ef5ec
Cyiox <cyioxox@gmail.com>
2025-04-21 10:29:18 -0400

    again

--- src/pages/Coding.jsx
+++ src/pages/Coding.jsx
@@ -3,7 +3,7 @@ function Coding(){
     return(
         <div>
         <h1>This is the coding page</h1>
-        <p>test<a href="https://docs.google.com/document/d/1tK5L2WUpE6oyDGRKBffNBIShDTxNklgwvpx6FEIhZII/edit?usp=sharing"></a></p>
+        <p>https://docs.google.com/document/d/1tK5L2WUpE6oyDGRKBffNBIShDTxNklgwvpx6FEIhZII/edit?usp=sharing</p>
         </div>
     )
 }
